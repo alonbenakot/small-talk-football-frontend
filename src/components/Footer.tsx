471b7ceb000464ea429b665b71f0a1511a8ef473
Alon <alon.benakot@gmail.com>
2025-01-24 21:55:39 +0200

    installations and header/footer components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,6 @@
+const Footer = () => {
+  return (
+    <footer>&copy; Alon Benakot 2025</footer>
+  )
+}
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+const Header = () => {
+  return (
+    <header>Small Talk Football Header</header>
+
+  )
+}
+export default Header;
