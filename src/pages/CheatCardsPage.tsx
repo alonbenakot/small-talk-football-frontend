ee282200aed9903fe5335c1c0f92290aafee18c5
Alon <alon.benakot@gmail.com>
2025-04-16 22:08:26 +0300

    feat: add api calls in cheatCardsPage and return child components.

--- src/pages/CheatCardsPage.tsx
+++ src/pages/CheatCardsPage.tsx
@@ -1,22 +1,49 @@
 import { useEffect } from "react";
 import useApi from "../utils/hooks/use-api.ts";
 import { getCheatCardCategories, getCheatCards } from "../utils/api/http.ts";
+import CheatCardsByCategories
+  from "../components/features/cheat-cards/cheat-card-categories/CheatCardsByCategories.tsx";
+import CheatCard from "../components/features/cheat-cards/cheat-card/CheatCard.tsx";
 
 const CheatCardsPage = () => {
-  const {fetchedData: categories, isLoading, error, invokeApi: invokeCategoriesApi} = useApi(getCheatCardCategories);
-  const {fetchedData: cheatCards, invokeApi: invokeCheatCardsApi} = useApi(getCheatCards);
+  const {
+    fetchedData: categories,
+    isLoading: isCategoriesLoading,
+    error: categoriesError,
+    invokeApi: invokeCategoriesApi
+  } = useApi(getCheatCardCategories);
+  const {
+    fetchedData: cheatCards,
+    isLoading: isCheatCardsLoading,
+    error: cheatCardsError,
+    invokeApi: invokeCheatCardsApi
+  } = useApi(getCheatCards);
+
   useEffect(() => {
     const fetchData = async () => {
-      await invokeCategoriesApi();
-      await invokeCheatCardsApi();
+      await Promise.all([invokeCategoriesApi(), invokeCheatCardsApi()]);
     };
     fetchData();
-  }, [invokeCategoriesApi, invokeCheatCardsApi] );
+  }, [invokeCategoriesApi, invokeCheatCardsApi]);
+
+  const allApiCallsOk = !isCheatCardsLoading && !isCategoriesLoading
+    && !cheatCardsError && !categoriesError;
 
   return (
     <>
       <h2>Cheat Cards</h2>
-
+      <div>
+        { allApiCallsOk && categories && cheatCards &&
+          <CheatCardsByCategories
+            categories={ categories.data }
+            cheatCards={ cheatCards.data }
+            selectedCardCategory={ categories.data[0] }
+          /> }
+      </div>
+      <div>
+        { !isCheatCardsLoading && !cheatCardsError && cheatCards &&
+        <CheatCard {...cheatCards.data[0]}/> }
+      </div>
     </>
   )
 }
