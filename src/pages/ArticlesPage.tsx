563a3d10a12bbc24fa9cf747a7c46dcb0ca8e2eb
Alon <alon.benakot@gmail.com>
2025-06-09 22:22:13 +0300

    chore: move to router loader - not complete

--- src/pages/ArticlesPage.tsx
+++ src/pages/ArticlesPage.tsx
@@ -2,36 +2,34 @@ import Button from "../components/ui/button/Button.tsx";
 import ArticlesList from "../components/features/articles/ArticlesList.tsx";
 import { useAuthStore } from "../store/store.ts";
 import { useEffect, useState } from "react";
-import useApi from "../utils/hooks/use-api.ts";
-import { getPendingArticles, getPublishedArticles } from "../utils/api/http.ts";
-import ArticleModel from "../components/features/articles/models/ArticleModel.ts";
 import Loader from "../components/ui/loader/Loader.tsx";
 import ErrorBlock from "../components/ui/error-block/ErrorBlock.tsx";
+import { useLoaderData, useNavigate, useNavigation, useSearchParams } from "react-router-dom";
+import { formatString } from "../utils/FormatUtil.ts";
+import { ArticleLoaderOutput } from "../routes/loaders/ArticleLoader.ts";
 
-export type ArticleFilter = 'Published' | 'Pending';
+export type ArticleFilter = 'published' | 'pending';
 
-function deriveButtonText(articlesFilter: "Published" | "Pending") {
-  return articlesFilter === 'Pending' ? 'Published' : 'Pending';
+function deriveButtonText(articlesFilter: ArticleFilter) {
+  return articlesFilter === 'pending' ? 'published' : 'pending';
 }
 
 const ArticlesPage = () => {
   const {selectedUser} = useAuthStore();
-  const [filter, setFilter] = useState<ArticleFilter>('Published');
-  const {
-    invokeApi,
-    fetchedData: articles,
-    isLoading,
-    error
-  } = useApi<ArticleModel[]>(filter === 'Published' ? getPublishedArticles : getPendingArticles);
+  const [searchParams] = useSearchParams();
+  const initialFilter: ArticleFilter = (searchParams.get("filter") as ArticleFilter) || "published";
+  const [filter, setFilter] = useState<ArticleFilter>(initialFilter);
+  const navigation = useNavigation();
+  const navigate = useNavigate();
+  const {data: articles, error} = useLoaderData<ArticleLoaderOutput>();
+  const isLoading = navigation.state === "loading";
 
   useEffect(() => {
-    const fetchData = async () => {
-      await invokeApi();
-    };
-    fetchData();
-  }, [filter, invokeApi]);
-
-  const buttonText = deriveButtonText(filter);
+    const current = searchParams.get("filter");
+    if (current !== filter) {
+      navigate(`?filter=${ filter }`, {replace: true});
+    }
+  }, [filter, searchParams, navigate]);
 
   const handleArticleToggle = () => {
     setFilter((prevState) => deriveButtonText(prevState));
@@ -40,7 +38,7 @@ const ArticlesPage = () => {
   return (
     <div className="max-w-5xl mx-auto px-4 py-8 space-y-6">
       <h2 className="text-3xl font-bold mb-3 text-gray-800 text-center">
-        { `${ filter } Articles` }
+        { `${ formatString(filter) } Articles` }
       </h2>
       { isLoading &&
         <div className="flex justify-center items-center w-full h-40">
@@ -50,7 +48,7 @@ const ArticlesPage = () => {
 
       { error &&
         <div className="flex justify-center items-center w-full h-40">
-          <ErrorBlock title={ "ERROR_TITLE" } message={ "ERROR_MSG" }/>
+          <ErrorBlock title={ "Article Error" } message={ error }/>
         </div>
       }
 
@@ -59,12 +57,12 @@ const ArticlesPage = () => {
           <div className="flex flex-col items-center justify-between gap-4">
             { selectedUser?.userIndications.pendingArticles && (
               <Button buttonType="primary" onClick={ handleArticleToggle }>
-                { buttonText }
+                { deriveButtonText(filter) }
               </Button>
             ) }
           </div>
 
-          <ArticlesList articles={ articles?.data }/>
+          <ArticlesList articles={ articles }/>
         </>
       ) }
     </div>
--- src/routes/loaders/ArticleLoader.ts
+++ src/routes/loaders/ArticleLoader.ts
@@ -1,6 +1,26 @@
-import type { LoaderFunction } from "react-router-dom";
-import { getPublishedArticles } from "../../utils/api/http.ts";
+import { LoaderFunctionArgs } from "react-router-dom";
+import { getPendingArticles, getPublishedArticles } from "../../utils/api/http";
+import ArticleModel from "../../components/features/articles/models/ArticleModel.ts";
 
-export const articlesLoader: LoaderFunction = () => {
-  return getPublishedArticles();
+export type ArticleLoaderOutput = {
+  data: ArticleModel[];
+  error: string | null;
+  loading: boolean;
+};
+
+export const articlesLoader = async ({request}: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const filter = url.searchParams.get("filter") ?? "published";
+
+  try {
+    const articlesApi = filter === "pending" ? getPendingArticles : getPublishedArticles;
+    const data = await articlesApi();
+    return {data: data.data, error: null, loading: false};
+  } catch (err: unknown) {
+    let errorMessage = "Failed to load articles";
+    if (err instanceof Error) {
+      errorMessage = err.message;
+    }
+    return {data: [], error: errorMessage, loading: false};
+  }
 };
