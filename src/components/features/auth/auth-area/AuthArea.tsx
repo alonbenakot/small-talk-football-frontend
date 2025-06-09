0bdd7bcd88f1df5a285b698468ffd7c9df3da95f
Alon <alon.benakot@gmail.com>
2025-06-09 11:16:23 +0300

    use useAuthStore in AuthArea

--- src/components/features/auth/auth-area/AuthArea.tsx
+++ src/components/features/auth/auth-area/AuthArea.tsx
@@ -1,13 +1,12 @@
 import LoggedInArea from "../logged-in-area/LoggedInArea.tsx";
 import LoggedOutArea from "../logged-out-area/LoggedOutArea.tsx";
-import { useSelector } from "react-redux";
-import { RootState } from "../../../../store/store.ts";
+import { useAuthStore } from "../../../../store/store.ts";
 
 const AuthArea = () => {
-  const user = useSelector((state: RootState) => state.auth.user);
+  const {selectedUser} = useAuthStore();
 
   return (
-    user ? <LoggedInArea/> : <LoggedOutArea/>
+    selectedUser ? <LoggedInArea/> : <LoggedOutArea/>
   )
 }
 export default AuthArea;
