479ef387e8b91c649b9fdc41c68065b2ddee8c3c
Alon <alon.benakot@gmail.com>
2025-08-05 23:33:32 +0300

    feat: create mobileNavbar; hide header links on desktops

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,6 +1,7 @@
 import Header from "../components/ui/header/Header.tsx";
 import { Outlet, useNavigation } from "react-router-dom";
 import GlobalSpinner from "../components/ui/spinner/GlobalSpinner.tsx";
+import MobileNavbar from "../components/ui/mobile-navbar/MobileNavbar.tsx";
 
 const RootLayout = () => {
   const navigation = useNavigation();
@@ -9,16 +10,13 @@ const RootLayout = () => {
   return (
     <div className="relative min-h-screen w-full overflow-x-hidden">
       <div className="fixed inset-0 -z-10 bg-[url('/grass-background.png')] bg-bottom bg-cover"></div>
-
       <div className="grid min-h-screen grid-rows-[auto_1fr] sm:grid-rows-[auto_5fr]">
-        <Header />
-
+        <Header/>
         <main className="p-4 sm:p-6 md:p-8 overflow-y-auto relative">
-          <GlobalSpinner isNavigating={isNavigating} />
-          <Outlet />
+          <GlobalSpinner isNavigating={ isNavigating }/>
+          <Outlet/>
         </main>
-
-        {/* <Footer /> */}
+        <MobileNavbar/>
       </div>
     </div>
   );
